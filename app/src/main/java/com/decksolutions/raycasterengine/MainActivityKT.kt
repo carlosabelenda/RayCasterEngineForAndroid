e5f0b7d59c264a22183e0d5c081895a9efa336a8
Carlos <carlosdeckmagia@gmail.com>
2022-08-13 11:49:16 +0200

    Project adapted to Kotlin. MainActivity created in Kotlin

---
+++ app/src/main/java/com/decksolutions/raycasterengine/MainActivityKT.kt
@@ -0,0 +1,32 @@
+package com.decksolutions.raycasterengine
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import com.decksolutions.raycasterengine.R
+import com.decksolutions.raycasterengine.SurfaceViewRayCasting
+
+class MainActivityKT : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //SurfaceViewCrystal c = new SurfaceViewCrystal(this);
+        //c.init(c.getWidth(),c.getHeight());
+        val c = SurfaceViewRayCasting(this.applicationContext)
+        setContentView(c)
+        try {
+            c.prepareScreenView()
+        } catch (e: Exception) {
+            // TODO Auto-generated catch block
+            e.printStackTrace()
+        }
+        Log.d("TEST", "OnCreate")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+}
